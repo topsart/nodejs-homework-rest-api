e6a405aff6ad4b98c1ccc09bc16d3df8170d8b4f
topsart <64708421+topsart@users.noreply.github.com>
2021-09-17 16:26:02 +0300

    add gravatar

--- controllers/users/signup.js
+++ controllers/users/signup.js
@@ -1,19 +1,18 @@
-const fs = require('fs/promises');
-const path = require('path');
+const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const { Conflict } = require('http-errors');
 const { User } = require('../../models');
 
-const avatarsDir = path.join(__dirname, '../../', 'public/avatars');
-
 const signup = async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password } = req.body;  
+  const avatarUrl = gravatar.url(email, { s: '200', protocol: 'http' });
+
   const user = await User.findOne({ email });
   if (user) {
     throw new Conflict('Email in use');
   }
   const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-  const result = await User.create({ email, password: hashPassword });
+  const result = await User.create({ email, password: hashPassword, avatarUrl });
   res.status(201).json({
     status: 'success',
     code: 201,
